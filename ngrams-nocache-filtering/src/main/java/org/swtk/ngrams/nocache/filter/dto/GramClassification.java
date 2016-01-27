16b5198e3603b560a89c95ab5b083f0da3f10d6b
Craig Trim <cmtrim@us.ibm.com>
2016-01-27 11:24:32 -0800

    update gram classifications

--- ngrams-nocache-filtering/src/main/java/org/swtk/ngrams/nocache/filter/dto/GramClassification.java
+++ ngrams-nocache-filtering/src/main/java/org/swtk/ngrams/nocache/filter/dto/GramClassification.java
@@ -2,5 +2,19 @@ package org.swtk.ngrams.nocache.filter.dto;
 
 public enum GramClassification {
 
-	GNGRAM, PUNCTUATION, STOPWORD, WIKTIONARY, WORDNET;
+	GNGRAM,
+
+	NOISE_MODEL_01,
+
+	NOISE_MODEL_02,
+
+	NOISE_MODEL_03,
+
+	PUNCTUATION,
+
+	STOPWORD,
+
+	WIKTIONARY,
+
+	WORDNET;
 }
